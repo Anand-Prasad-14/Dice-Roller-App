162a4d7ed22c1800d3c2170f5a3a8cd1523e8c0a
anand prasad <adrock4321anandgp@gmail.com>
2022-11-23 11:48:05 +0530

    main.kt

---
+++ main.kt
@@ -0,0 +1,22 @@
+fun main() {
+    println("anand")
+    val age : Int =
+    if(age == 42){
+        println("42 is a ratnesh")
+    }
+    else{
+        println("not matched")
+
+}
+
+val stringLenghtFunc: (String) -> Int = {input ->
+ input.lenght
+}
+val strlen: Int = stringLenghtFunc("android")
+println(strlen)
+
+
+
+
+
+}
